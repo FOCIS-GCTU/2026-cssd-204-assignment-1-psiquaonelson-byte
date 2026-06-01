dfa642a83ce8fb5c59bc00fc44e78453bfa74a07
psiquaonelson-byte <psiquaonelson@gmail.com>
2026-06-01 23:23:42 +0000

    Completed Assignment

--- initials.py
+++ initials.py
@@ -2,6 +2,37 @@
 def main():
   pass #code goes here
 
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Prince
+# STUDENT ID:  2425404187
+# Email: psiquaonelson@gmail.com
+# Grader: Augustus
+#
+# On my honor, Prince, this programming assignment is my own work
+# I have not provided this code to any other student.
 
+
+def main():
+    # Print initials P, S, I, Q, N in stylized large block letters.
+    # Each letter is 12 characters wide and 10 characters high,
+    # followed by a 4-asterisk period block, with 3 leading dots.
+
+    print()
+    print("...PSIQN")
+    print()
+    print("...PPPPPPPPPP..****SSSSSSSSSS..****IIIIIIIIIIII****.QQQQQQQQQQ.****NN........NN****")
+    print("...PP......PPPP****SS..........****....IIII....****QQ........QQ****NNN.......NN****")
+    print("...PP......PPPP****SS..........****....IIII....****QQ........QQ****NNNN......NN****")
+    print("...PPPPPPPPPP..****SSSSSSSSSS..****....IIII....****QQ........QQ****NN.NN.....NN****")
+    print("...PPPPPPPPPP..****SSSSSSSSSS..****....IIII....****QQ........QQ****NN..NN....NN****")
+    print("...PP..........****..........SS****....IIII....****QQ....QQ..QQ****NN...NN...NN****")
+    print("...PP..........****..........SS****....IIII....****QQ....QQQQQQ****NN....NN..NN****")
+    print("...PP..........****..........SS****....IIII....****QQ.....QQQQQ****NN.....NNNN.****")
+    print("...PP..........****SS........SS****....IIII....****QQ........QQ****NN......NNN.****")
+    print("...PP..........****SSSSSSSSSS..****IIIIIIIIIIII****.QQQQQQQQQQ.****NN.......NN.****")
+    print()
 
 main()
